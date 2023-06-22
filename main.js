d8834ec44f84ee886a26dee39b354ee4f992e76b
Anton <anton.l.ahlstrom@gmail.com>
2023-06-22 15:57:07 +0200

    Getting towards functioning game

--- main.js
+++ main.js
@@ -1,54 +1,78 @@
+// Generate a random computer choise
+
 function getComputerChoise() {
     let randomNumber = Math.floor(Math.random() * 3)
     switch (randomNumber) {
         case 0:
-            return ("Rock")
-            break
+            return ("rock");
+            break;
         case 1:
-            return ("Paper")
-            break
+            return ("paper");
+            break;
         case 2:
-            return ("Scissors")
+            return ("scissors");
+            break;
     }
 }
+// Determine tie, win or loss
+
+// Note: You could probably refactor this even further, for example to use comparisions better ("if (playerSelection == computerSelection), tie") etc. Maybe assin an integrer to each choise to easier keep track and determine if a choise is a win or loss?
+
+function playRound() {
+    computerSelection = getComputerChoise();
+    userinput = prompt("Choose Rock, Paper or Scissors!");
+    let playerSelection = userinput.toLowerCase();
+    // Lowercase players input. Not sure if this works, if bugs, start here
 
 
-// A probably neater way to handle the above
-function playRoundAlt(playerSelection, computerSelection) {
     // Handle all cases where computer selects "Rock"
-    if (computerSelection == "Rock") {
-        if (playerSelection == "Rock") {
-            console.log("Tie!");
+    if (computerSelection == "rock") {
+        if (playerSelection == "rock") {
+            return ("Tie!");
         }
-        else if (playerSelection = "Scissors") {
-            console.log("You lost!");
+        else if (playerSelection = "scissors") {
+            return ("You lost!");
         }
         else {
-            console.log("You won!");
+            return ("You won!");
         }
     }
     // Handle all cases where compute selects "Paper"
-    else if (computerSelection == "Paper") {
-        if (playerSelection == "Paper") {
-            console.log("Tie!");
+    else if (computerSelection == "paper") {
+        if (playerSelection == "paper") {
+            return ("Tie!");
         }
-        else if (playerSelection == "Rock") {
-            console.log("You lost!");
+        else if (playerSelection == "rock") {
+            return ("You lost!");
         }
         else {
-            console.log("You won!");
+            return ("You won!");
         }
     }
-    // Handle all cases where computer selects "Scissors" (doesn't need to be explicitly typed out)
-    else {
-        if (playerSelection == "Scissors") {
-            console.log("Tie!");
+    // Handle all cases where computer selects "Scissors"
+    else if (computerSelection == "scissors") {
+        if (playerSelection == "scissors") {
+            return ("Tie!");
         }
-        else if (playerSelection == "Paper") {
-            console.log("You won!");
+        else if (playerSelection == "paper") {
+            return ("You won!");
         }
         else {
-            console.log("You lost!");
+            return ("You lost!");
         }
+    }
+    // Handle misspellings, etc.
+    else {
+        return ("Please choose Rock, Paper or Scissors");
+    }
+    console.log(playerSelection, computerSelection);
+}
+
+function game() {
+    let numberOfRoundsPlayed = 0;
+    while (numberOfRoundsPlayed < 5) {
+        playRound();
+        console.log(playRound() + " Computer choose " + computerSelection);
+        numberOfRoundsPlayed++;
     }
 }
