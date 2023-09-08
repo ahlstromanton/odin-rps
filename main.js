2ef82d0d9c5aad226bd969b277279f36f6a314f8
Anton <anton.l.ahlstrom@gmail.com>
2023-09-08 11:14:28 +0200

    wip

--- main.js
+++ main.js
@@ -10,8 +10,35 @@ function getComputerChoise() {
     else {
         computerSelection = "Scissors";
     }
-    return computerSelection;
+    return computerSelection.toLowerCase();
 }
-function playSelection() {
-    
+
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection == playerSelection) {
+        return "Tie!";
+    } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+    ) {
+        return "You lose :(";
+    } else {
+        return "You win! :)";
+    }
 }
+
+function game() {
+    let rounds = 5;
+    for (let i = 0; i < rounds; i++) {
+        const playerSelection = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
+        const computerSelection = getComputerChoise();
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+    }
+    return "Call the function game() again to play again :)"
+}
+
+// const playerSelection = "rock"; //Change this to user input later
+// const computerSelection = getComputerChoise();
+// console.log(playRound(playerSelection, computerSelection));
