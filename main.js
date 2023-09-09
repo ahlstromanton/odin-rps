ffd9ddcac662ffeda412f04e9e2956780059eb68
anton <anton.l.ahlstrom@gmail.com>
2023-09-10 01:36:57 +0200

    wip

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoise() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    let computerSelection;
-    if (randomNumber == 0) {
-        computerSelection = "Rock";
-    }
-    else if (randomNumber == 1) {
-        computerSelection = "Paper";
-    }
-    else {
-        computerSelection = "Scissors";
-    }
-    return computerSelection.toLowerCase();
+function game() {
+    const computerSelection = getComputerChoise();
+    playRound(playerSelection, computerSelection);
+    showScores();
+    showOutcome(playerSelection, computerSelection);
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -30,11 +22,19 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    const computerSelection = getComputerChoise();
-    playRound(playerSelection, computerSelection);
-    showScores();
-    showOutcome(playerSelection, computerSelection);
+function getComputerChoise() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerSelection;
+    if (randomNumber == 0) {
+        computerSelection = "Rock";
+    }
+    else if (randomNumber == 1) {
+        computerSelection = "Paper";
+    }
+    else {
+        computerSelection = "Scissors";
+    }
+    return computerSelection.toLowerCase();
 }
 
 function showScores() {
@@ -51,6 +51,11 @@ function showOutcome(playerSelection, computerSelection) {
     computerChoise.textContent = computerSelection;
 }
 
+function gameOver() {
+    const gameOverText = document.querySelector('#gameOver');
+    //Change this to either just insert the text when if statement is true, or remove by adding a empty string in the block below.
+    gameOverText.textContent = "GAME OVER! Press any button to start again."; //Show "game over" and final score + option to reset
+}
 
 const rockBtn = document.querySelector('.buttons #rock');
 rockBtn.onclick = () => {
